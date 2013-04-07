3c520700d9ab6f1c6dc1f2559af679eae7ee43bc
axtanh3 <axtanh3@gmail.com>
2013-04-07 04:12:51 -0700

    Create fibonacy.c

---
+++ fibonacy.c
@@ -0,0 +1,24 @@
+/*
+ * Fibonacy
+ */
+#include <stdio.h>
+
+/* prototype */
+int f(int n);
+/* main function */
+int main(void)
+{
+  int n = 3;
+	printf("%d", f(n));
+	return 0;
+}
+
+int f(int n)
+{
+	if (n == 0)
+		return 0;
+	else if (n == 1)
+		return 1;
+	else 
+		return (f(n - 1) + f(n - 2));
+}
